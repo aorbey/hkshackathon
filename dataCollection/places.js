10ccaaa91c7e2dd347ad278118dca08242984432
smn1022 <zck1022@hotmail.com>
2015-02-21 15:12:06 -0500

    more data
    more data

--- dataCollection/places.js
+++ dataCollection/places.js
@@ -1,46 +1,89 @@
 eqfeed_callback({
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -72.651863,
-                    42.236164
-                ]
-            },
-            "properties": {
-                "name": "The Log Cabin",
-                "likes": 8084
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -72.607475,
-                    42.206596
-                ]
-            },
-            "properties": {
-                "name": "Holyoke City Hall",
-                "likes": 1566
-            }
-        },{
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -72.618431,
-                    42.203629
-                ]
-            },
-            "properties": {
-                "name": "Holyoke Senior Center",
-                "likes": 54
-            }
-        }
-    ]
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -72.651863,
+          42.236164
+        ]
+      },
+      "properties": {
+        "name": "The Log Cabin",
+        "likes": 8084
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -72.607475,
+          42.206596
+        ]
+      },
+      "properties": {
+        "name": "Holyoke City Hall",
+        "likes": 1566
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -72.618431,
+          42.203629
+        ]
+      },
+      "properties": {
+        "name": "Holyoke Senior Center",
+        "likes": 54
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -72.613657,
+          42.203422
+        ]
+      },
+      "properties": {
+        "name": "Holyoke Public Library",
+        "likes": 1545
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -72.608676,
+          42.203668
+        ]
+      },
+      "properties": {
+        "name": "Propark America",
+        "likes": 222
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -72.606194,
+          42.206807
+        ]
+      },
+      "properties": {
+        "name": "Ernest E. Proulx Municipal Parking Facility",
+        "likes": 0
+      }
+    }
+  ]
 });
