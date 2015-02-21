e9bd4f356cbfd2f90cfeef3918d9059c14713733
smn1022 <zck1022@hotmail.com>
2015-02-21 15:32:39 -0500

    new data
    bus stop to garage

--- dataCollection/places.js
+++ dataCollection/places.js
@@ -6,83 +6,25 @@ eqfeed_callback({
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -72.651863,
-          42.236164
-        ]
-      },
-      "properties": {
-        "name": "The Log Cabin",
-        "likes": 8084
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -72.607475,
-          42.206596
-        ]
-      },
-      "properties": {
-        "name": "Holyoke City Hall",
-        "likes": 1566
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -72.618431,
-          42.203629
-        ]
-      },
-      "properties": {
-        "name": "Holyoke Senior Center",
-        "likes": 54
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -72.613657,
-          42.203422
-        ]
-      },
-      "properties": {
-        "name": "Holyoke Public Library",
-        "likes": 1545
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -72.608676,
-          42.203668
-        ]
-      },
-      "properties": {
-        "name": "Propark America",
-        "likes": 222
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
           -72.606194,
           42.206807
         ]
       },
       "properties": {
-        "name": "Ernest E. Proulx Municipal Parking Facility",
-        "likes": 0
+        "name": "Ernest E. Proulx Municipal Parking Facility"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -72.6141523,
+          42.2016129
+        ]
+      },
+      "properties": {
+        "name": "Bus Lines (B23 / P20 / P21 /R22 / R24)"
       }
     }
   ]
